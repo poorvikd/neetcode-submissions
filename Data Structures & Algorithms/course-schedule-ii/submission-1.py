212549b574b95170c9892c0ab0a8a6fa093a4d2e
poorvikd <64829799+poorvikd@users.noreply.github.com>
2026-05-18 12:20:03 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,58 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indeg = defaultdict(lambda: 0)
+        outdeg = defaultdict(lambda: 0)
+        adj = defaultdict(list)
+        for a,b in prerequisites:
+            indeg[a] += 1
+            outdeg[b] += 1
+            adj[b].append(a)
+
+        # visited = [0 for i in range(numCourses)]
+        # inStack = [0 for i in range(numCourses)]
+        # def dfs(node):
+        #     if visited[node] == 1 and inStack[node] == 1:
+        #         return False
+        #     elif visited[node] == 1  and inStack[node] == 0:
+        #         return True
+
+        #     inStack[node] = 1
+        #     visited[node] = 1
+        #     resp = True
+        #     for neighbor in adj[node]:
+        #         resp &= dfs(neighbor)
+            
+        #     inStack[node] = 0
+        #     return resp
+        # queue = deque()
+        # for i in range(numCourses):
+        #     if visited[i] == 0:
+        #         hasCycle = dfs(i)
+        #         if hasCycle == False:
+        #             return []
+        #     if indeg[i] == 0:
+        #         queue.append((i,0))
+        
+        # output = []
+        # # visited = [0 for i in range(numCourses)]
+        # while queue:
+        #     node, level = queue.pop()
+        #     output.append(node)
+        #     for neighbor in adj[node]:
+        #         queue.append((neighbor,level+1))
+        
+        # return output
+
+        queue = deque(i for i in range(numCourses) if indeg[i] == 0)
+        output = []
+        # visited = [0 for i in range(numCourses)]
+        while queue:
+            node = queue.pop()
+            output.append(node)
+            for neighbor in adj[node]:
+                indeg[neighbor] -= 1
+                if indeg[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return output if len(output) == numCourses else []
+                    
